2d929715f0c5a6d8f1203c35f38bcc62243ce2ba
unknown <you@example.com>
2023-06-26 13:59:06 +0100

    OC:Added Stuff

---
+++ worldex.sql
@@ -0,0 +1,14 @@
+USE world;
+
+SELECT LifeExpectancy, Name  from country
+ORDER BY LifeExpectancy DESC;
+
+
+/*SELECT COUNT(CountryCode) from city
+WHERE CountryCode = 'USA';*/
+
+/*SELECT Population, LifeExpectancy FROM Country
+WHERE Name ='Argentina';*/
+
+
+
